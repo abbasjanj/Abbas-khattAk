5c58c8d8daf3e0fe9eca16625da9fe00376204f1
abbasjanj <azharkarwan313@gmail.com>
2025-01-01 05:26:15 +0500

    Update qr.js

--- qr.js
+++ qr.js
@@ -17,14 +17,14 @@ const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
 ┌───⭓『
-❒ *WASI-MD*
+❒ *abbas-MD*
 ❒ _NOW DEPLOY IT_
 └────────────⭓
 ┌───⭓
 ❒  • Chat with owner •
-❒ *GitHub:* __https://github.com/WASI-MD_
-❒ *Author:* _wa.me/923192173398_
-❒ *YT:* _https://youtube.com/@wasitech10_
+❒ *GitHub:* __https://www.facebook.com/share/v/1EExtJaLph/_
+❒ *Author:* _https://www.facebook.com/share/v/1EExtJaLph/_
+❒ *YT:* _https://www.facebook.com/share/v/1EExtJaLph/_
 └────────────⭓
 `
 
